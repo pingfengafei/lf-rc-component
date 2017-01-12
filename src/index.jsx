07021000cbaceff6a84adb6bf6a80aa60029dadf
pingfengafei <intern@maxent-inc.com>
2017-01-12 15:20:42 +0800

    添加新文件

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+/**
+ * Created by pingfengafei on 16/12/23.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './layout/home';
+import './style/index.less';
+require('font-awesome/css/font-awesome.css');
+
+
+ReactDOM.render(<Home></Home>, document.getElementsByClassName('root')[0]);
